bdc73bc596516e733f199ffb95b1cd69a76f4acf
Gabriel Koch Fodi <67654630+gabrikf@users.noreply.github.com>
2021-09-23 13:11:51 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,22 +2,22 @@ const mysql = require('mysql2/promise')
 require('dotenv').config()
 
 
-// //with connection pool
-// module.exports = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-//   port:process.env.DB_PORT,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// }) 
-
-module.exports = mysql.createConnection({
+//with connection pool
+module.exports = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port:process.env.DB_PORT,
-})  
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+}) 
+
+// module.exports = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port:process.env.DB_PORT,
+// })  
