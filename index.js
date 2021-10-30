e45d19390a68acbba4d27566b6b3638ad8d5e703
Gabriel Koch Fodi <gabrielkochfodi@gmail.com>
2021-10-30 17:16:41 -0300

    removing unused codes

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const routes = require('./routes')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors())
-    next();
-});
-app.use(routes)
-app.use(express.json())
-
-app.use(cors())
-
-
-
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 app.listen(3000, (err) => {
-  
-  if(err){
-    console.log('Not possible to listen on port 3000')
-  }else{
-    console.log('Running on port 3000')
+  if (err) {
+    console.log("Not possible to listen on port 3000");
+  } else {
+    console.log("Running on port 3000");
   }
-})
+});
